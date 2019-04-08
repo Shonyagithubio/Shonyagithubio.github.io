43405683dd864e7f8c20c76a7b4d78784d921ac6
Shonyarohan <shonya2106@gmail.com>
2019-04-08 13:10:37 +0200

    databasevurdering

---
+++ databasevurdering/javascript.js
@@ -0,0 +1,60 @@
+//Referanser til HTML-elementer
+const main = document.querySelector("main");
+
+
+//Skriv referanser til database under her:
+const db = firebase.database();
+const kaker = db.ref("Varer/Kaker");
+const boller = db.ref("Varer/Boller");
+const brød = db.ref("Varer/Brød");
+
+// Javascript-funksjon for å generere HTML-kode for varer
+function genererHTML(snapshot){
+  let varer = snapshot.val();
+  console.log(varer);
+  main.innerHTML +=`
+      <article class="Varer">
+        <h1>${varer.Navn}</h1>
+        <img src="bilder/${varer.Bilde}" alt="">
+        <p>Pris: ${varer.Pris}</p>
+        <p>Type: ${varer.Type}</p>
+      </article>
+  `;
+}
+
+// Funksjon som viser alle varer
+function visAlle(){
+  main.innerHTML = ""; // sletter innholdet i main.
+  kaker.on("child_added",genererHTML);
+  boller.on("child_added", genererHTML);
+  brød.on("child_added", genererHTML);
+}
+function visKaker(){
+  main.innerHTML = ``;
+  kaker.orderByChild("Navn").on("child_added", genererHTML);
+}
+function visBursdag(){
+  main.innerHTML = ``;
+  kaker.orderByChild("Type").equalTo("Bursdag").on("child_added", genererHTML);
+}
+function visKonfirmasjon(){
+  main.innerHTML = ``;
+  kaker.orderByChild("Type").equalTo("Konfirmasjon").on("child_added", genererHTML);
+}
+function visBryllup(){
+  main.innerHTML = ``;
+  kaker.orderByChild("Type").equalTo("Bryllup").on("child_added", genererHTML);
+}
+function visBoller(){
+  main.innerHTML = ``;
+  boller.orderByChild("Navn").on("child_added", genererHTML);
+}
+function visGlutenfri(){
+  main.innerHTML = ``;
+  boller.orderByChild("Type").equalTo("Glutenfri").on("child_added", genererHTML);
+    brød.orderByChild("Type").equalTo("Glutenfri").on("child_added", genererHTML);
+}
+function visBrød(){
+  main.innerHTML = ``;
+  brød.orderByChild("Navn").on("child_added", genererHTML);
+}
